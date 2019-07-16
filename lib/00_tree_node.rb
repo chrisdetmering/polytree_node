94fefe4e72f8c39d8c41bf0e798f686c198533b8
froggy <chrisdetmering@gmail.com>
2019-07-16 11:11:21 -0700

    Added parent, children and value methods

--- lib/00_tree_node.rb
+++ lib/00_tree_node.rb
@@ -1,3 +1,26 @@
 class PolyTreeNode
 
+    def initialize(value)
+        @value = value
+        @parent = nil 
+        @children = []
+    end 
+
+    def parent
+        @parent
+    end 
+
+    def children
+       @children
+    end 
+
+    def value 
+        @value
+    end 
+
+    def parent=(value)
+        @parent = value 
+        parent = PolyTreeNode.new
+    end 
+
 end
